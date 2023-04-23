408673574a5a73f4b6c7169aa0707046688b549c
Zayyan Naveed <zayyan.naveed@yale.edu>
2023-04-23 15:47:35 -0400

    Merged game and sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
 let host = "cpsc484-03.yale.internal:8888";
-let score = 0;
 
 let frames = {
   socket: null,
@@ -49,179 +48,255 @@ let frames = {
     };
 
     for (let part in body_index) {
-      let index = body_index[part];
-      let x = (frame.people[0].joints[index].pixel.x)/5;
-      let y = (frame.people[0].joints[index].pixel.y)/5;
-      // let z = frame.people[0].joints[index].position.z;
-      coordinates[part] = [x, y]
-      console.log(part + ": " + coordinates[part]);
+        let index = body_index[part];
+        let x = window.innerWidth - (frame.people[0].joints[index].pixel.x);
+        let y = (frame.people[0].joints[index].pixel.y);
+        // let z = frame.people[0].joints[index].position.z;
+        coordinates[part] = [x, y]
+        console.log(part + ": " + coordinates[part]);
+      }
+        //hardcoded coordinates for testing
+        // coordinates['PELVIS'] = [100, 200];
+        // coordinates['NECK'] = [100, 150];
+        // coordinates['HEAD'] = [100, 100];
+        // coordinates['EAR_LEFT'] = [65, 90];
+        // coordinates['EAR_RIGHT'] = [135, 90];
+        // coordinates['SHOULDER_LEFT'] = [75, 140];
+        // coordinates['ELBOW_LEFT'] = [50, 140];
+        // coordinates['WRIST_LEFT'] = [30, 140];
+        // coordinates['SHOULDER_RIGHT'] = [125, 140];
+        // coordinates['ELBOW_RIGHT'] = [150, 140];
+        // coordinates['WRIST_RIGHT'] = [170, 140];
+        // coordinates['HIP_LEFT'] = [80, 220];
+        // coordinates['KNEE_LEFT'] = [80, 260];
+        // coordinates['ANKLE_LEFT'] = [80, 290];  
+        // coordinates['HIP_RIGHT'] = [120, 220];
+        // coordinates['KNEE_RIGHT'] = [120, 260];
+        // coordinates['ANKLE_RIGHT'] = [120, 290];
+        // coordinates['FOOT_LEFT'] = [75, 310];
+        // coordinates['FOOT_RIGHT'] = [125, 310];
+        // console.log(coordinates);
+  
+      return coordinates;
     }
-      //hardcoded coordinates for testing
-      // coordinates['PELVIS'] = [100, 200];
-      // coordinates['NECK'] = [100, 150];
-      // coordinates['HEAD'] = [100, 100];
-      // coordinates['EAR_LEFT'] = [65, 90];
-      // coordinates['EAR_RIGHT'] = [135, 90];
-      // coordinates['SHOULDER_LEFT'] = [75, 140];
-      // coordinates['ELBOW_LEFT'] = [50, 140];
-      // coordinates['WRIST_LEFT'] = [30, 140];
-      // coordinates['SHOULDER_RIGHT'] = [125, 140];
-      // coordinates['ELBOW_RIGHT'] = [150, 140];
-      // coordinates['WRIST_RIGHT'] = [170, 140];
-      // coordinates['HIP_LEFT'] = [80, 220];
-      // coordinates['KNEE_LEFT'] = [80, 260];
-      // coordinates['ANKLE_LEFT'] = [80, 290];  
-      // coordinates['HIP_RIGHT'] = [120, 220];
-      // coordinates['KNEE_RIGHT'] = [120, 260];
-      // coordinates['ANKLE_RIGHT'] = [120, 290];
-      // coordinates['FOOT_LEFT'] = [75, 310];
-      // coordinates['FOOT_RIGHT'] = [125, 310];
-      // console.log(coordinates);
-
-    return coordinates;
-  }
-};
-
-let twod = {
-  socket: null,
-
-  start: function() {
-    var url = "ws://" + host + "/twod";
-    twod.socket = new WebSocket(url);
-    twod.socket.onmessage = function(event) {
-      twod.show(JSON.parse(event.data));
+  };
+  
+  let twod = {
+    socket: null,
+  
+    start: function() {
+      var url = "ws://" + host + "/twod";
+      twod.socket = new WebSocket(url);
+      twod.socket.onmessage = function(event) {
+        twod.show(JSON.parse(event.data));
+      }
+    },
+  
+    show: function(twod) {
+      // Set the src of an element with id 'twod' instead of class 'twod'
+      $('#twod').attr("src", 'data:image/png;base64,'+twod.src);
     }
-  },
-
-  show: function(twod) {
-    // Set the src of an element with id 'twod' instead of class 'twod'
-    $('#twod').attr("src", 'data:image/png;base64,'+twod.src);
+  };
+  
+  function drawBody(coordinates) {
+    let canvas = document.getElementById("my-canvas");
+    let ctx = canvas.getContext("2d");
+    
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.strokeStyle = "rgba(64, 224, 208, 0.8)";
+    ctx.lineWidth = 10;
+    ctx.fillStyle = "rgba(64, 224, 208, 0.8)";
+    
+    // Draw the body
+    ctx.beginPath();
+  
+    let radius = (coordinates['EAR_RIGHT'][0] - coordinates['EAR_LEFT'][0]) / 2.5;
+    if (radius < 0) {
+      radius = -radius;
+    }
+    console.log("radius: " + radius)
+    // Draw the head
+    ctx.arc(coordinates['HEAD'][0], coordinates['HEAD'][1], radius, 0, 2*Math.PI);
+    
+    // Draw the torso
+    ctx.moveTo(coordinates['SHOULDER_LEFT'][0], coordinates['SHOULDER_LEFT'][1]);
+    ctx.lineTo(coordinates['SHOULDER_RIGHT'][0], coordinates['SHOULDER_RIGHT'][1]);
+    ctx.lineTo(coordinates['HIP_RIGHT'][0], coordinates['HIP_RIGHT'][1]);
+    ctx.lineTo(coordinates['HIP_LEFT'][0], coordinates['HIP_LEFT'][1]);
+    ctx.lineTo(coordinates['SHOULDER_LEFT'][0], coordinates['SHOULDER_LEFT'][1]);
+    
+    // Draw the left arm
+    ctx.moveTo(coordinates['SHOULDER_LEFT'][0], coordinates['SHOULDER_LEFT'][1]);
+    ctx.lineTo(coordinates['ELBOW_LEFT'][0], coordinates['ELBOW_LEFT'][1]);
+    ctx.lineTo(coordinates['WRIST_LEFT'][0], coordinates['WRIST_LEFT'][1]);
+    
+    // Draw the right arm
+    ctx.moveTo(coordinates['SHOULDER_RIGHT'][0], coordinates['SHOULDER_RIGHT'][1]);
+    ctx.lineTo(coordinates['ELBOW_RIGHT'][0], coordinates['ELBOW_RIGHT'][1]);
+    ctx.lineTo(coordinates['WRIST_RIGHT'][0], coordinates['WRIST_RIGHT'][1]);
+    
+    // Draw the left leg
+    ctx.moveTo(coordinates['HIP_LEFT'][0], coordinates['HIP_LEFT'][1]);
+    ctx.lineTo(coordinates['KNEE_LEFT'][0], coordinates['KNEE_LEFT'][1]);
+    ctx.lineTo(coordinates['ANKLE_LEFT'][0], coordinates['ANKLE_LEFT'][1]);
+    
+    // Draw the right leg
+    ctx.moveTo(coordinates['HIP_RIGHT'][0], coordinates['HIP_RIGHT'][1]);
+    ctx.lineTo(coordinates['KNEE_RIGHT'][0], coordinates['KNEE_RIGHT'][1]);
+    ctx.lineTo(coordinates['ANKLE_RIGHT'][0], coordinates['ANKLE_RIGHT'][1]);
+  
+    // Stroke and fill the path
+    ctx.stroke();
+    ctx.fill();
   }
-};
 
-function drawBody(coordinates) {
-  let canvas = document.getElementById("my-canvas");
-  let ctx = canvas.getContext("2d");
-  
-  // Clear the canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.strokeStyle = "rgba(64, 224, 208, 0.8)";
-  ctx.lineWidth = 10;
-  ctx.fillStyle = "rgba(64, 224, 208, 0.8)";
-  
-  // Draw the body
-  ctx.beginPath();
+  class Scene1 extends Phaser.Scene {
+    constructor () {
+        super('startGame');
+    }
 
-  let radius = (coordinates['EAR_RIGHT'][0] - coordinates['EAR_LEFT'][0]) / 2.5;
-  if (radius < 0) {
-    radius = -radius;
-  }
-  console.log("radius: " + radius)
-  // Draw the head
-  ctx.arc(coordinates['HEAD'][0], coordinates['HEAD'][1], radius, 0, 2*Math.PI);
-  
-  // Draw the torso
-  ctx.moveTo(coordinates['SHOULDER_LEFT'][0], coordinates['SHOULDER_LEFT'][1]);
-  ctx.lineTo(coordinates['SHOULDER_RIGHT'][0], coordinates['SHOULDER_RIGHT'][1]);
-  ctx.lineTo(coordinates['HIP_RIGHT'][0], coordinates['HIP_RIGHT'][1]);
-  ctx.lineTo(coordinates['HIP_LEFT'][0], coordinates['HIP_LEFT'][1]);
-  ctx.lineTo(coordinates['SHOULDER_LEFT'][0], coordinates['SHOULDER_LEFT'][1]);
-  
-  // Draw the left arm
-  ctx.moveTo(coordinates['SHOULDER_LEFT'][0], coordinates['SHOULDER_LEFT'][1]);
-  ctx.lineTo(coordinates['ELBOW_LEFT'][0], coordinates['ELBOW_LEFT'][1]);
-  ctx.lineTo(coordinates['WRIST_LEFT'][0], coordinates['WRIST_LEFT'][1]);
-  
-  // Draw the right arm
-  ctx.moveTo(coordinates['SHOULDER_RIGHT'][0], coordinates['SHOULDER_RIGHT'][1]);
-  ctx.lineTo(coordinates['ELBOW_RIGHT'][0], coordinates['ELBOW_RIGHT'][1]);
-  ctx.lineTo(coordinates['WRIST_RIGHT'][0], coordinates['WRIST_RIGHT'][1]);
-  
-  // Draw the left leg
-  ctx.moveTo(coordinates['HIP_LEFT'][0], coordinates['HIP_LEFT'][1]);
-  ctx.lineTo(coordinates['KNEE_LEFT'][0], coordinates['KNEE_LEFT'][1]);
-  ctx.lineTo(coordinates['ANKLE_LEFT'][0], coordinates['ANKLE_LEFT'][1]);
-  
-  // Draw the right leg
-  ctx.moveTo(coordinates['HIP_RIGHT'][0], coordinates['HIP_RIGHT'][1]);
-  ctx.lineTo(coordinates['KNEE_RIGHT'][0], coordinates['KNEE_RIGHT'][1]);
-  ctx.lineTo(coordinates['ANKLE_RIGHT'][0], coordinates['ANKLE_RIGHT'][1]);
+    preload() {
+        this.load.image("background", "assets/images/hole-bg.png");
+    }
+    create() {
+        // this.add.text(100, 100, "Loading game...");
+        this.background = this.add.image(0, 0, "background");
+        this.background.setOrigin(0, 0);
+        this.background.displayHeight = window.innerHeight;
+        this.background.displayWidth = window.innerWidth;
+        // this.background.scaleX = window.innerWidth;
 
-  // Stroke and fill the path
-  ctx.stroke();
-  ctx.fill();
+        this.add.text(430,300, "HOLE IN THE WALL", {
+            font: "70px Arial", 
+            fill: "#ff0044"
+        });
+        this.add.text(500,450, "RAISE YOUR HAND TO PLAY", {
+            font: "30px Arial", 
+            fill: "yellow"
+        });
+
+        //start game
+        this.input.keyboard.on('keydown-ENTER', () => {
+            this.scene.start('playGame');
+          });
+    }
 }
 
-function animateWall(duration) {
-  const start = performance.now();
-  const wall = document.getElementById('wall');
-  const box = document.getElementById('box');
-  const wallRatio = 5;
-  const baseWidth = parseFloat(getComputedStyle(box).width);
-
-
-  function animate() {
-    const elapsed = performance.now() - start;
-    // console.log(`This is the elapsed time ${elapsed/1000}s`); //convert to timer
-    const progress = Math.min(elapsed / duration, 1);
-    const scale = progress.toFixed(2);
-    // console.log(`This is the scale ${scale} at ${elapsed/1000}s`);
-    const box_scale = (progress + 0.5).toFixed(2);
-    // console.log(`This is the box scale ${box_scale} at ${elapsed/1000}s`)
-
-    wall.style.width = `${scale * 100}%`;
-    wall.style.height = `${scale * 100}%`;
-    box.style.width = `${box_scale* 10}%`;
-    box.style.height = `${box_scale * 30}%`;
-    // console.log(`This is the wall height ${wall.style.height}`);
-    // console.log(`This is the box height ${box.style.height}`);
-
-    if (progress < 1) {
-      window.requestAnimationFrame(animate);
+class Scene2 extends Phaser.Scene {
+    constructor () {
+        super('playGame');
     }
-  }
 
-  window.requestAnimationFrame(animate);
-}
-
-function updateScore() {
-  const scoreElement = document.getElementById('score');
-  score += 1;
-  scoreElement.innerHTML = `Score: ${score}`;
-}
-
-function startModal() {
-  var modal = document.getElementById("myModal");
-  // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-  // display instructions
-  modal.style.display = "block";
-
-  // When the user clicks on the button, open the modal
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-    animateDiv(7000);
-  }
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+    preload() {
+        this.load.image('wall', 'assets/images/wall.webp');
+        this.load.image('allblack', 'assets/images/hole.webp');
     }
-    animateDiv(7000);
-  }
+    create() {
+        frames.start();
+        this.tile = this.add.sprite(window.innerWidth / 2, window.innerHeight / 2, 'wall');
+        // tile.setOrigin(0.5);
+        // this.tile.displayWidth = window.innerWidth/1000;
+        // this.tile.displayHeight = window.innerHeight/1000;
+        this.tile.scaleX = 0.1;
+        this.tile.scaleY = 0.1;
+        this.tile.depth = 0;
+
+        // create a new graphics object
+        this.graphics = this.add.graphics();
+
+        // set the fill color and alpha
+        this.graphics.fillStyle(0x000000, 1);
+        this.graphics.lineStyle(4, 0x000000, 1);
+
+        // draw the human body shape using the graphics object
+        this.graphics.beginPath();
+
+        //draw head
+        this.graphics.arc(0, -100, 50, Phaser.Math.DegToRad(0), Phaser.Math.DegToRad(360), false);
+        this.graphics.moveTo(0, -50);
+        this.graphics.lineTo(80, -50);
+
+        //draw right arm
+        this.graphics.lineTo(200, -50);
+        this.graphics.lineTo(200, -20);
+        this.graphics.lineTo(80, -20);
+
+        //draw right leg
+        this.graphics.lineTo(80, 150);
+        this.graphics.lineTo(80, 300);
+        this.graphics.lineTo(50, 300);
+        this.graphics.lineTo(50, 150);
+
+
+        //draw left leg
+        this.graphics.lineTo(-50, 150);
+        this.graphics.lineTo(-50, 300);
+        this.graphics.lineTo(-80, 300);
+        this.graphics.lineTo(-80, 150);
+
+        //draw left arm
+        this.graphics.lineTo(-80, -20);
+        this.graphics.lineTo(-200, -20);
+        this.graphics.lineTo(-200, -50);
+        this.graphics.lineTo(-80, -50);
+
+        this.graphics.lineTo(0, -50);
+        this.graphics.closePath();
+        this.graphics.strokePath();
+        this.graphics.fillPath();
+        this.graphics.depth = 2;
+
+        this.graphics.x = window.innerWidth / 2;
+        this.graphics.y = window.innerHeight / 2;
+        this.graphics.scaleX = 0.1;
+        this.graphics.scaleY = 0.1;
+
+
+        this.graphics.setInteractive();
+        // this.graphics.on('pointerdown', function(pointer) {
+        //     check if the graphics object contains the pointer position
+        //     if (this.graphics.getBounds().contains(pointer.x, pointer.y)) {
+        //       console.log('clicked on graphics');
+        //     }
+        //   }, this);
+
+        this.graphics.setInteractive();
+        this.graphics.on('pointerdown', function(pointer) {
+            console.log('Graphics clicked!');
+        }, this);
+
+    }
+    update() {
+
+        if (this.tile.displayWidth < window.innerWidth) {
+            // this.tile.displayHeight += window.innerHeight/250;
+            // this.tile.displayWidth += window.innerWidth/250;
+            this.tile.scaleX += 0.005;
+            this.tile.scaleY += 0.005;
+            this.graphics.scaleX += 0.005;
+            this.graphics.scaleY += 0.005;
+        }
+        
+    }
 }
+
+
+const config = {
+    type:  Phaser.AUTO,
+    height: window.innerHeight,
+    width: window.innerWidth,
+    backgroundColor: '#FFFFFF',
+    scene: [Scene1, Scene2]
+}
+
+window.onload = function() {
+    var game = new Phaser.Game(config);
+}
+
 
 $(document).ready(function() {
-  frames.start();
   twod.start();
-  startModal();
-  updateScore();
 });
+
+

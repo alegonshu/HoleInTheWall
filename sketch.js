bab06548c0ef7e9805b6dca11af5cab9883516e4
Zayyan Naveed <zayyan.naveed@yale.edu>
2023-04-24 21:29:16 -0400

    Updated bodyid to track 1 person

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 let host = "cpsc484-03.yale.internal:8888";
-var body_coor = {};
+var body_coor = null;
 var body_color = "rgba(64, 224, 208, 0.8) ";
 let verify = false;
-
+var bodyID = null;
 let body_index = {
   'PELVIS': 0,
   'NECK': 3,
@@ -36,57 +36,69 @@ let frames = {
     let url = "ws://" + host + "/frames";
     frames.socket = new WebSocket(url);
     frames.socket.onmessage = function (event) {
-      let coordinates = frames.get_body_coordinates(JSON.parse(event.data));
-      body_coor = coordinates;
-      if (coordinates !== null) {
-        // console.log("Drawing body");
-        drawBody(coordinates);
+
+      if (bodyID === null) {
+        bodyID = frames.get_body_id(JSON.parse(event.data));
       }
       else {
-        console.log("No coordinates to draw");
+        let coordinates = frames.get_body_coordinates(JSON.parse(event.data));
+        body_coor = coordinates;
+        if (coordinates !== null && coordinates !== {}) {
+          
+          drawBody(coordinates);
+        }
+        else {
+          console.log("No coordinates to draw");
+        }
       }
     }
   },
 
+  get_body_id: function (frame) {
+    if (frame.people.length < 1) {
+      console.log("No body id");
+      return null;
+    }
+    else{
+      let body_id = frame.people[0].body_id;
+      let closest = frame.people[0].joints[0].position.z;
+      for (person of frame.people) {
+        // console.log(person);
+      if (person.joints[0].position.z < closest) {
+        body_id = person.body_id;
+        closest = person.joints[0].position.z;
+      }
+    }
+    console.log(`Changed body_id to ${body_id}`);
+    return body_id;
+    }
+    
+  },
+
   get_body_coordinates: function (frame) {
     let coordinates = {};
+
     if (frame.people.length < 1) {
-        console.log("No body detected");
-        return null;
+      console.log("No body detected");
+      bodyID = null;
+      return null;
     }
+    for (person of frame.people) {
 
-    for (let part in body_index) {
-        let index = body_index[part];
-        let x = window.innerWidth - (frame.people[0].joints[index].pixel.x);
-        let y = (frame.people[0].joints[index].pixel.y);
-        coordinates[part] = [x, y]
+      if (person.body_id === bodyID) {
+        for (let part in body_index) {
+            let index = body_index[part];
+            let x = window.innerWidth - (person.joints[index].pixel.x);
+            let y = (person.joints[index].pixel.y);
+            coordinates[part] = [x, y]
+          }
       }
-        // hardcoded coordinates for testing
-        // coordinates['PELVIS'] = [100, 200];
-        // coordinates['NECK'] = [100, 150];
-        // coordinates['HEAD'] = [100, 100];
-        // coordinates['EAR_LEFT'] = [65, 90];
-        // coordinates['EAR_RIGHT'] = [135, 90];
-        // coordinates['SHOULDER_LEFT'] = [75, 140];
-        // coordinates['ELBOW_LEFT'] = [50, 140];
-        // coordinates['WRIST_LEFT'] = [30, 140];
-        // coordinates['SHOULDER_RIGHT'] = [125, 140];
-        // coordinates['ELBOW_RIGHT'] = [150, 140];
-        // coordinates['WRIST_RIGHT'] = [100, 140];
-        // coordinates['HIP_LEFT'] = [80, 220];
-        // coordinates['KNEE_LEFT'] = [80, 260];
-        // coordinates['ANKLE_LEFT'] = [80, 200];  
-        // coordinates['HIP_RIGHT'] = [120, 220];
-        // coordinates['KNEE_RIGHT'] = [120, 260];
-        // coordinates['ANKLE_RIGHT'] = [120, 290];
-        // coordinates['FOOT_LEFT'] = [75, 310];
-        // coordinates['FOOT_RIGHT'] = [125, 310];
-
-        // for (let part in coordinates) {
-        //   coordinates[part][0] += 620;
-        //   coordinates[part][1] += 300;
-        // console.log(coordinates);
-        // }
+    }
+    console.log(coordinates);
+      if (coordinates === {}) {
+        bodyID = null;
+        return null;
+      }
       return coordinates;
     }
   };
@@ -125,7 +137,6 @@ let frames = {
     if (radius < 0) {
       radius = -radius;
     }
-    // console.log("radius: " + radius)
     // Draw the head
     ctx.arc(coordinates['HEAD'][0], coordinates['HEAD'][1], radius, 0, 2*Math.PI);
     
@@ -184,17 +195,12 @@ let frames = {
             fill: "yellow"
         });
         
-        // console.log(body_coor[7][1], body_coor[12][1]);
-        // start game
-        // if (handCheck()) {
-        //     this.scene.start('playGame');
-        // }
         this.input.keyboard.on('keydown-ENTER', () => {
             this.scene.start('Instructions');
           });
     }
     update() {
-        console.log(verify);
+        // console.log(verify);
         if (handCheck()) {
             setTimeout(() => {
                 if (verify) {
@@ -207,77 +213,123 @@ let frames = {
 class Scene2 extends Phaser.Scene {
     constructor () {
         super('Instructions');
+
+        this.hole = new Phaser.Geom.Polygon([
+          500, 300,
+          800, 300,
+          800, 900,
+          500, 900,
+        ]);
+
     }
 
     preload() {
         this.load.image("background", "assets/images/hole-bg.png");
     }
     create() {
-        // this.add.text(100, 100, "Loading game...");
-        this.background = this.add.image(0, 0, "background");
-        this.background.setOrigin(0, 0);
-        this.background.displayHeight = window.innerHeight;
-        this.background.displayWidth = window.innerWidth;
-        // this.background.scaleX = window.innerWidth;
+        // this.background = this.add.image(0, 0, "background");
+        // this.background.setOrigin(0, 0);
+        // this.background.displayHeight = window.innerHeight;
+        // this.background.displayWidth = window.innerWidth;
 
-        this.add.text(300,300, "A red wall with a black hole will begin to move towards you.", {
+        this.add.text(450,30, "INSTRUCTIONS", {
+          font: "50px Arial", 
+          fill: "#000000"
+      });
+
+        this.add.text(400,100, "A brick wall with a black hole will appear on the screen.", {
             font: "20px Arial", 
             fill: "#ff0044"
         });
-        this.add.text(300,400, "Position yourself such that you fit through the hole.", {
+        this.add.text(400,130, "Position yourself such that you fit through the hole by the end of the time.", {
             font: "20px Arial", 
             fill: "#ff0044"
         });
-        this.add.text(300,500, "Be quick and precise to avoid being hit by the walls.", {
+        this.add.text(400,160, "Be quick and precise to avoid being hit by the walls.", {
             font: "20px Arial", 
             fill: "#ff0044"
         });
-        this.add.text(300,600, "Good luck and have fun!", {
+        this.add.text(400,190, "Good luck and have fun!", {
             font: "20px Arial", 
             fill: "#ff0044"
         });
+        this.add.text(450,220, "Position yourself in the hole to begin", {
+          font: "25px Arial", 
+          fill: "#000000"
+      });
           
-        
-        // console.log(body_coor[7][1], body_coor[12][1]);
-        // start game
-        // if (handCheck()) {
-        //     this.scene.start('playGame');
-        // }
+        this.graphics = this.add.graphics();
+        this.graphics.depth = 2;
+        this.graphics.scaleX = 1;
+        this.graphics.scaleY = 1;
+
+        this.graphics.fillStyle(0x000000, 1);
+        this.graphics.lineStyle(4, 0x000000, 1);
+        this.graphics.fillPoints(this.hole.points, true);
+
+
         this.input.keyboard.on('keydown-ENTER', () => {
             this.scene.start('playGame');
           });
     }
     update() {
-            setTimeout(() => {
-                this.scene.start('playGame');
-            }, 5000);
+
+      let state = true;
+
+      if (body_coor != null) {
+
+        console.log(this.hole.points);
+
+        for (let part in body_index) {
+          let current_point = new Phaser.Geom.Point(body_coor[part][0], body_coor[part][1]);
+
+          if (!Phaser.Geom.Polygon.ContainsPoint(this.hole, current_point)) 
+          {
+            state = false;
+          }
         }
+      }
+      else {
+        state = false;
+      }
+      if (state == true) {
+        console.log("You are in the hole");
+        this.scene.start('playGame');
+
+        }
+    }
 }
 class Scene3 extends Phaser.Scene {
     constructor () {
         super('playGame');
         var hole_coor = hole_coordinates()
-        this.hole = new Phaser.Geom.Polygon(hole_coor);
+        // this.body1 = new Phaser.Geom.Polygon(hole_coor);
 
 
-      //   this.body1 = new Phaser.Geom.Polygon([
-      //     60, -40,
-      //     250,-40,
-      //     250,0,
-      //     60,0,
-      //     60,200,
-      //     60,380,
-      //     10,380,
-      //     10,200,
-      //     -10,200,
-      //     -10,380,
-      //     -60,380,
-      //     -60,200,
-      //     -60,0,
-      //     -250,0,
-      //     -250,-40,
-      //     -60,-40,
-      // ]);
+        // this.body1 = new Phaser.Geom.Polygon([
+        //   300, 300,
+        //   1000, 300,
+        //   1000, 900,
+        //   300, 900,
+        // ]);
+        this.body1 = new Phaser.Geom.Polygon([
+          60 + 700, -40 + 300,
+          250 + 700, -40 + 300,
+          250 + 700, 0 + 300,
+          60 + 700, 0 + 300,
+          60 + 700, 200 + 300,
+          60 + 700, 380 + 300,
+          10 + 700, 380 + 300,
+          10 + 700, 200 + 300,
+          -10 + 700, 200 + 300,
+          -10 + 700, 380 + 300,
+          -60 + 700, 380 + 300,
+          -60 + 700, 200 + 300,
+          -60 + 700, 0 + 300,
+          -250 + 700, 0 + 300,
+          -250 + 700, -40 + 300,
+          -60 + 700, -40 + 300,
+      ]);
 
       //   this.head = new Phaser.Geom.Circle(0, -100,60);
       // this.hole = new Hole(this);
@@ -302,14 +354,14 @@ class Scene3 extends Phaser.Scene {
         this.graphics = this.add.graphics();
         this.graphics.setName("hole");
         this.graphics.depth = 2;
-        this.graphics.x = window.innerWidth / 2;
-        this.graphics.y = window.innerHeight /2;
+        // this.graphics.x = window.innerWidth / 2;
+        // this.graphics.y = window.innerHeight /2;
         this.graphics.scaleX = 1;
         this.graphics.scaleY = 1;
 
         this.graphics.fillStyle(0x000000, 1);
         this.graphics.lineStyle(4, 0x000000, 1);
-        this.graphics.fillPoints(this.hole.points, true);
+        this.graphics.fillPoints(this.body1.points, true);
 
         // create a new graphics object
         // const rect = new Phaser.Geom.Rectangle(-150, 0, 300, 570);
@@ -335,21 +387,59 @@ class Scene3 extends Phaser.Scene {
         // this.graphics.scaleX = 0.1;
         // this.graphics.scaleY = 0.1;
 
+    }
+
+    update() {
+        var hole = this.children.getByName("hole");
+        let state = true;
+
+        if (body_coor != null) {
+
+          console.log(this.body1.points);
+
+          for (let part in body_index) {
+            let current_point = new Phaser.Geom.Point(body_coor[part][0], body_coor[part][1]);
+
+            if (!Phaser.Geom.Polygon.ContainsPoint(this.body1, current_point)) 
+                  // && !Phaser.Geom.Circle.ContainsPoint(this.head, current_point)
+            {
+              state = false;
+              console.log(`${part}:${current_point.x}) not in hole`);
+            }
+            else {
+              console.log(`${part}:(${current_point.x}) in hole`);
+            }
+          }
+
+          if (state) {
+            body_color = "rgba(0, 0, 250, 0.8)";
+          }
+          else {
+            body_color = "rgba(64, 224, 208 , 0.8)";
+          }
+        }
 
     }
 }
 
 function handCheck() {
-    if ((body_coor != undefined) && (body_coor !== null) && (body_coor !== {}) && (Object.keys(body_coor.length !== 0))) {
-            if (body_coor['WRIST_RIGHT'][1] < body_coor['SHOULDER_RIGHT'][1]) {
-                verify = true;
-                return true;
+    if ((body_coor != undefined) && (body_coor != null) && (body_coor != {}) && (Object.keys(body_coor.length != 0))) {
+            if  (body_coor['WRIST_RIGHT'][1] != undefined && body_coor['WRIST_LEFT'][1] != undefined 
+                  && body_coor['SHOULDER_RIGHT'][1] != undefined && body_coor['SHOULDER_LEFT'][1] != undefined) 
+            {
+                if ((body_coor['WRIST_RIGHT'][1] < body_coor['SHOULDER_RIGHT'][1]) 
+                || (body_coor['WRIST_LEFT'][1] < body_coor['SHOULDER_LEFT'][1])) 
+                {
+                    verify = true;
+                    return true;
+                }
+                else {
+                    verify = false;
+                    return false;
+              }
             }
-            else {
-                return false;
-            }
-          
-    }
+  }
+    verify = false;
     return false;
 }
 
